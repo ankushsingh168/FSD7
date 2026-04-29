ed14cc5777c8d18beacde2090f68533d660351f9
ankushsingh168 <ankushsinghbaghel881@gmail.com>
2026-04-29 23:55:10 +0530

    Add privacy policy, terms of service, and sitemap pages; update footer links and enhance navigation

---
+++ jsLoops/script.js
@@ -0,0 +1,27 @@
+for(var i=1; i<=10; i++){
+
+    var a=2;
+    console.log(a*i);
+
+}
+
+let b=[24,58,33,86,96,12,53,23,32,100];
+for(var j=0; j<10; j++)
+{
+      console.log(b[j]);
+
+}
+
+let k=1;
+while(k<=10){
+    var c=2;
+    console.log(c*k);
+    k++;
+}
+
+let l=1;
+do{
+    var d=3;
+    console.log(c*k);
+    k++;
+}while(m<=10)
