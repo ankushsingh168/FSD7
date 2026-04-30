223866343c302c017c0f3529e04237bba9592112
ankushsingh168 <ankushsinghbaghel881@gmail.com>
2026-04-30 17:13:41 +0530

    Remove outdated JavaScript files and add new assignments for loops and basic JavaScript functionality

---
+++ JavaScript/ForLoopAssignment1/script.js
@@ -0,0 +1,64 @@
+for(var i=1; i<=5; i++)
+{ 
+    console.log(i);
+}
+
+for(var i=1; i<=10; i++)
+{
+    console.log(i)
+    
+}
+
+for(var i=10; i>=1; i--)
+{
+    console.log(i)
+}
+
+for(var i=2; i<=10; i++)
+{ 
+    if(i%2 == 0)
+    console.log(i);
+}
+
+for(var i=1; i<=9; i++)
+{ 
+    if(i%2 == 1)
+    console.log(i);
+}
+for(var i=1; i<=10; i++)
+{
+    var a=15;
+    console.log(a*[i])
+}
+
+for(var i=1; i<=20; i++)
+{
+    if (i % 2 !== 0)
+    {
+         console.log(i)
+
+    }
+   
+}
+
+for(var i=5; i<=50; i++)
+{
+     if ((i-5) % 5 == 0)
+    {
+         console.log(i)
+
+    }
+}
+
+for(var i=1; i<=10; i++)
+{
+    console.log(i*i)
+}
+
+for(var i=3; i<=10; i++)
+{
+    if(i%3 == 0)
+    console.log(i)
+}
+
+
---
+++ JavaScript/WhileloopAssignment/script.js
@@ -0,0 +1,32 @@
+let i=1;
+while(i<=5)
+{
+    console.log(i);
+    i++;
+}
+
+let j=5;
+while(j>=1)
+{
+    console.log(j);
+    j--;
+}
+
+
+let k=2;
+while(k<=11)
+{
+    console.log(k);
+    k+=2;
+}
+
+let l = 10;
+let sum = 0;
+
+while (l >= 5)
+ {
+    sum += l;
+    l--;
+}
+
+console.log(sum);
