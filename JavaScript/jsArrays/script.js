77554d197df6578d844521e97b8d838db65c41ce
ankushsingh168 <ankushsinghbaghel881@gmail.com>
2026-05-05 19:49:47 +0530

    Add form fields for user input and enhance submission functionality

---
+++ JavaScript/jsArrays/script.js
@@ -0,0 +1,52 @@
+// let arr = [12, 23, 34, 45, 67, 78, 12];
+// for (var i = 0; i < 7; i++) {
+//   console.log([i]);
+// }
+
+// console.log(arr);
+// arr[2] = arr[2] + 10;
+// console.log(arr);
+// arr = [];
+// console.log(arr);
+
+let arr = [12, 23, 34, 45, 56, 67, 78, 89, 90];
+console.log(arr);
+
+arr.push(99);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.unshift(11);
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+
+console.log(arr.at(4));
+
+console.log(arr.indexOf(78));
+
+console.log(arr.includes(89));
+
+console.log(arr.indexOf(89));
+
+let unar = [56, 23, 89, 45, 67, 34, 12, 90, 87, 45, 98];
+console.log(unar);
+
+unar.sort();
+console.log(unar);
+
+unar.reverse();
+console.log(unar);
+
+
+let ar = [56, 23, 89, 45, 67, 34, 12];
+for (var i = 0; i < 7; i++) {
+  console.log(ar[i]);
+}
+console.log("for each loop");
+
+
+ar.forEach((element) => {console.log(element);});
--- JavaScript/jsDOM/script.js
+++ JavaScript/jsDOM/script.js
@@ -1,14 +1,37 @@
 function Submit() {
   console.log("submit button clicked");
   const fn = document.getElementById("fullName").value;
+  const fn1 = document.getElementById("city").value;
+  const fn2 = document.getElementById("email").value;
+  const fn3 = document.getElementById("password").value;
+
   console.log(fn);
+  console.log(fn1);
+  console.log(fn2);
+  console.log(fn3);
+  
 
-  document.getElementById("mydata").innerText = fn;
-   document.getElementById("mydata2").innerText = fn;
-  document.getElementById("fullName").value = "";
+  // document.getElementById("mydata").innerText = fn;
+  // document.getElementById("fullName").value = "";
 
-  document.getElementById("dataCard").classList.add("divsShow");
-   document.getElementById("dataCard").classList.remove("divHide");
+  // document.getElementById("dataCard").classList.add("divsShow");
+  // document.getElementById("dataCard").classList.remove("divHide");
 
+  // document.getElementById("mydata1").innerText = fn;
+  // document.getElementById("fullName").value = "";
+
+  // document.getElementById("dataCard1").classList.add("divsShow");
+  // document.getElementById("dataCard1").classList.remove("divHide");
+
+  // document.getElementById("mydata2").innerText = fn;
+  // document.getElementById("fullName").value = "";
+
+  // document.getElementById("dataCard2").classList.add("divsShow");
+  // document.getElementById("dataCard2").classList.remove("divHide");
+
+  // document.getElementById("mydata3").innerText = fn;
+  // document.getElementById("fullName").value = "";
+
+  // document.getElementById("dataCard3").classList.add("divsShow");
+  // document.getElementById("dataCard3").classList.remove("divHide");
 }
-
