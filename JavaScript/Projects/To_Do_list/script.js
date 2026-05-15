4c7aff21f860904f7c7bb55dd615918983eb0136
ankushsingh168 <ankushsinghbaghel881@gmail.com>
2026-05-15 20:27:34 +0530

    Fix form ID typo and enhance Coaching Institute Registration Form with required fields and section headers

--- JavaScript/Projects/To_Do_list/script.js
+++ JavaScript/Projects/To_Do_list/script.js
@@ -1,6 +1,64 @@
-function addNewTask(){
-    const taskToAdd = document.getElementById("newTask").value
+function addNewTask() {
+  const taskToAdd = document.getElementById("newTask").value;
 
-    const LI =document.createElement("li")
-    LI.classList.add("my-3")
+  const LI = document.createElement("li");
+  LI.classList.add("my-3");
+
+  const DIV = document.createElement("div");
+  DIV.classList.add("d-flex", "align-items-center");
+
+  const SPAN1 = document.createElement("span");
+  SPAN1.innerText = taskToAdd;
+
+  const BUTTON = document.createElement("button");
+  BUTTON.classList.add(
+    "btn",
+    "btn-danger",
+    "ms-4",
+    "d-flex",
+    "gap-3",
+    "align-items-center",
+    "justify-content-center",
+  );
+  BUTTON.onclick = () => LI.remove();
+
+  const I = document.createElement("i");
+  I.classList.add("bi", "bi-trash");
+
+  const SPAN2 = document.createElement("span");
+  SPAN2.innerText = "Delete";
+
+  BUTTON.appendChild(I);
+  BUTTON.appendChild(SPAN2);
+
+  DIV.appendChild(SPAN1);
+  DIV.appendChild(BUTTON);
+
+  LI.appendChild(DIV);
+
+  document.getElementById("taskDiv").classList.remove("d-none");
+
+  document.getElementById("taskList").appendChild(LI);
+
+  document.getElementById("newTask").value = "";
 }
+
+// function addNewTask() {
+//     saveToLocalStorage(taskToAdd)
+//     document.getElementById("newTask").value ="",
+
+// function saveToLocalStorage(taskToAdd) {
+//   const oldTaskArray = JSON.parse(localStorage.getItem("toDoTask")) || [];
+
+//   TaskArray.push(taskToAdd);
+
+//   console.log(TaskArray);
+
+//   const newTaskArray = JSON.stringify(TaskArray);
+
+//   localStorage.setItem("to do task", newTaskArray);
+// }
+// function saveToLocalStorage(taskToAdd) {
+//   const TaskArray = JSON.parse(localStorage.getItem("toDoTask")) || [];
+// }
+// }
