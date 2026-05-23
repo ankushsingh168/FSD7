9f811c85d63241014075f851058dace7148b4e1d
ankushsingh168 <ankushsinghbaghel881@gmail.com>
2026-05-23 20:56:49 +0530

    Add initial HTML structure, JavaScript function for joke retrieval, and basic styling

---
+++ JavaScript/Projects/jokeApp/script.js
@@ -0,0 +1,12 @@
+async function GetNewJoke() {
+  const API_URL = "https://v2.jokeapi.dev/joke/Programming";
+
+  const response = await fetch(API_URL);
+
+  const data = await response.json();
+
+  document.getElementById("setup").innerText = data.joke || data.setup;
+  document.getElementById("delivery").innerText = data.delivery || "";
+
+  document.getElementById("punchline").innerText = data.setup || data.punchline;
+}
