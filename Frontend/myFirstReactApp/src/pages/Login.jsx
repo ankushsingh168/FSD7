a85d50d54b77bd9e69514f283f223f59a2a2c546
ankushsingh168 <ankushsinghbaghel881@gmail.com>
2026-06-06 20:35:21 +0530

    feat: enhance Login and Register components with form handling and routing links

--- Frontend/myFirstReactApp/src/pages/Login.jsx
+++ Frontend/myFirstReactApp/src/pages/Login.jsx
@@ -1,12 +1,58 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 function Login() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("userName :", userName);
+    console.log("password :", password);
+  };
+
   return (
-    <>
-      <div className="container d-flex justify-content-center align-items-center"> Login
-        <span className="text-primary fs-2"></span>
+    <div className="d-flex justify-content-center">
+      <div className="border p-3 w-50 mt-5 bg-light shadow rounded">
+        <h1 className="text-center">Welcome Back!!!</h1>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="userName">Username</label>
+          <input
+            type="text"
+            name="userName"
+            className="form-control"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+
+          <br />
+
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <br />
+
+          <div className="text-end">
+            <button type="submit" className="btn btn-primary">
+              Login
+            </button>
+          </div>
+        </form>
+        <hr />
+        
+        <p>
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
       </div>
-      
-    </>
+    </div>
   );
 }
-
 export default Login;
--- Frontend/myFirstReactApp/src/pages/Register.jsx
+++ Frontend/myFirstReactApp/src/pages/Register.jsx
@@ -1,12 +1,71 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 function Register() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email , setUserEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("userName :", userName);
+    console.log("password :", password);
+    console.log("email: ", email);
+    
+  };
+
   return (
-    <>
-     <div className="container d-flex justify-content-center align-items-center"> Register
-        <span className="text-primary fs-2"></span>
+    
+      <div className="d-flex justify-content-center">
+        <div className="border p-3 w-50 mt-5 bg-light shadow rounded">
+          <h1 className="text-center">Registration!!!</h1>
+
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="userName">Full Name</label>
+            <input
+              type="text"
+              name="userName"
+              className="form-control"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+
+            <br />
+             <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="usermail"
+              className="form-control"
+              value={email}
+              onChange={(e) => setUserEmail(e.target.value)}
+            />
+            <br />
+
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <br />
+
+            <div className=" text-end" >
+              <button type="submit" className="btn btn-primary">
+                Register
+              </button>
+            </div>
+          </form>
+          <hr />
+           <p>
+            Don't have an account? <Link to ="/login" >Login</Link>
+          </p>
+        </div>
       </div>
-      
-    </>
+    
   );
 }
-
 export default Register;
