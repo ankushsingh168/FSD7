40bddf52ee01e63c031c175e550bf82cc138ad07
ankushsingh168 <ankushsinghbaghel881@gmail.com>
2026-06-08 20:22:14 +0530

    feat: initialize Vite project with React and Tailwind CSS
    - Added Vite configuration for React and Tailwind CSS.
    - Created main entry point for React application.
    - Added global CSS file with Tailwind CSS import.
    - Included Vite logo SVG asset.

---
+++ Cravings/src/App.jsx
@@ -0,0 +1,21 @@
+const App = () => {
+  return (
+    <>
+      <div className="bg-green-200 text-amber-800 p-3.25">
+        <div className="w-5xl mx-auto border p-20 bg-white/70 rounded-2xl flex flex-col justify-center items-center ">
+          <p className="text-green-700">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+            quaerat perferendis accusantium distinctio odio aut enim magni nemo
+            ipsa, harum aspernatur possimus? Unde ipsum, at maiores cumque
+            soluta quia harum?
+          </p>
+           <button className="bg-blue-500 px-5 py-2 mt-3 mx-5 rounded text-white hover:bg-blue-700 active:bg-amber-500">
+            submit</button>
+        </div>
+      </div>
+
+     
+    </>
+  );
+};
+export default App;
